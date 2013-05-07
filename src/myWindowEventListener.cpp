3b7a316a31e8adf30b382513c34a6d0e3d8894d9
VirtUal <virtualprotoss@yahoo.com>
2013-05-07 16:42:26 +1000

    Add WindowEventListener.

---
+++ src/myWindowEventListener.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   myWindowEventListener.cpp
+ * Author: virtual
+ * 
+ * Created on 7 Май 2013 г., 16:10
+ */
+
+#include "myWindowEventListener.h"
+
+myWindowEventListener::myWindowEventListener() {
+}
+
+myWindowEventListener::myWindowEventListener(const myWindowEventListener& orig) {
+}
+
+myWindowEventListener::~myWindowEventListener() {
+}
+
